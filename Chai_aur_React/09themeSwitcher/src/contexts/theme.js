698ab9d0ee6ee970d60e1294ed52d96195ddff7d
Sukhwinder-i0 <sukhwinder39i0@gmail.com>
2024-10-02 11:12:48 +0530

    tailwind config class

--- Chai_aur_React/09themeSwitcher/src/contexts/theme.js
+++ Chai_aur_React/09themeSwitcher/src/contexts/theme.js
@@ -10,7 +10,7 @@ export const ThemeContext = createContext({
 
 export const ThemeProvider = ThemeContext.Provider
 
-export default function useTheme () {
+export default function useTheme() {
     return useContext(ThemeContext)
 }
 

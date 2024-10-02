3dc7894c46563225cc60c6164dbf380ee285ab41
Sukhwinder-i0 <sukhwinder39i0@gmail.com>
2024-10-02 08:57:48 +0530

    theme switcher

--- Chai_aur_React/09themeSwitcher/src/contexts/theme.js
+++ Chai_aur_React/09themeSwitcher/src/contexts/theme.js
@@ -3,11 +3,15 @@ import { createContext, useContext } from "react";
 export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
-    lightTheme: () => {},
+    lightTheme: () => {}
 })
 
 
 
-export default ThemeProvider = ThemeContext.Provider
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme () {
+    return useContext(ThemeContext)
+}
 
 
